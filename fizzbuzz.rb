03fc11c5a9a299ddbe5373d88662c3d63cbc315a
David Markel <davidgmarkel@gmail.com>
2018-03-03 20:50:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 def fizzbuzz(number)
-  if  number % 3 == 0 && number % 5 == 0 
+  if  number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
   elseif number % 3 == 0
     "Fizz"
   elseif number % 5 == 0
     "Buzz"
   else
-    "Nil"
+    nil
   end
-
 end
